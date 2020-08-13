15cbf40e65e0f169cf5ca574d87a74cba9f92f1b
Ali Zhussupov <zhussupovali@gmail.com>
2020-08-13 18:41:18 +0600

    Initial commit

---
+++ BitriseDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitriseDemo
+//
+//  Created by Ali Zhussupov on 8/13/20.
+//  Copyright © 2020 Ali Zhussupov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-BitriseDemo/Pods-BitriseDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BitriseDemo : NSObject
+@end
+@implementation PodsDummy_Pods_BitriseDemo
+@end
